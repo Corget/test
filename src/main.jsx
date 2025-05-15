01e5c90d724f29e4fde3275d3fd8ae2b89f1a0ce
Corget <youssieftagour@gmail.com>
2025-05-15 11:24:35 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+// instead of BrowserRouter
+
 import App from '@/App';
 import '@/index.css';
 
